9b0be035dc4a160d216e8a3bbbeaa4c3964c1b8c
Guilherme <guisantana0204@gmail.com>
2020-02-19 21:00:17 -0300

    Criação das classes Retangulo, e iniciação da classe Retangulo Teste

---
+++ src/main/java/exercicios/Correntista.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Correntista {
+    public String codigo;
+    public String nome;
+    public String email;
+    public String telefone;
+
+    public String getDadosCorrentista() {
+        return nome + " " + codigo + " " + email + " " + telefone;
+    }
+}
